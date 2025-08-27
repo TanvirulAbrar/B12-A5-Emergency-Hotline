7d6cc9b93b833e8a421468186c5693c87ec7b173
TanvirulAbrar <abrartanvirul@gmail.com>
2025-08-27 23:11:08 +0600

    added copy system

--- home.js
+++ home.js
@@ -10,17 +10,17 @@ for (let btn of buttonBtn) {
   btn.addEventListener("click", function (e) {
     const heart = document.getElementById("total-coins");
     let total = Number(heart.innerText);
-    if (total > 19) {
-      total -= Number(20);
-      alert("colling");
-    } else {
-      alert("set massage");
-    }
-    heart.innerText = "" + total;
     let title = btn.parentNode.parentNode.childNodes[3].innerText;
     let num = btn.parentNode.parentNode.childNodes[7].innerText;
-    const time = new Date().toLocaleTimeString();
-    addChild(title, time, num);
+    if (total > 19) {
+      total -= Number(20);
+      alert("calling " + title + " " + num);
+      heart.innerText = "" + total;
+      const time = new Date().toLocaleTimeString();
+      addChild(title, time, num);
+    } else {
+      alert("coin is less than 20");
+    }
   });
 }
 function addChild(title, time, num) {
@@ -39,15 +39,25 @@ function addChild(title, time, num) {
   parent.appendChild(child);
 }
 
-function addCopy() {
-  //total-heart; total-coins; total-copy;
-  const heart = document.getElementById("total-copy");
-  let total = Number(heart.innerText) + 1;
-  heart.innerText = "" + total;
+const copyBtn = document.getElementsByClassName("copyButton");
+for (let btn of copyBtn) {
+  btn.addEventListener("click", function (e) {
+    //total-heart; total-coins; total-copy;
+    const heart = document.getElementById("total-copy");
+    let total = Number(heart.innerText) + 1;
+    heart.innerText = "" + total;
+    let num = btn.parentNode.parentNode.childNodes[7].innerText;
+    navigator.clipboard
+      .writeText(num)
+      .then(() => {
+        console.log("Copied:", num);
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
+  });
 }
 function clearHistory() {
   let parent = document.getElementById("parent");
-  for (let i of parent.childNodes) {
-    parent.remove(i);
-  }
+  parent.innerHTML = "";
 }
