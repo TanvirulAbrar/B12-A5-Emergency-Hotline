d55515bb3587cd1111cb57801fae9cec4af3774d
TanvirulAbrar <abrartanvirul@gmail.com>
2025-08-27 17:32:23 +0600

    adding cards

--- home.js
+++ home.js
@@ -4,20 +4,34 @@ function addHeart() {
   const total = Number(heart.innerText) + 1;
   heart.innerText = "" + total;
 }
-function addCoin() {
-  //total-heart; total-coins; total-copy;
-  const heart = document.getElementById("total-coins");
-  const total = Number(heart.innerText);
-  if (total > 19) {
-    total -= 20;
-  } else {
-    alert("set massage");
-  }
-  heart.innerText = "" + total;
+
+const buttonBtn = document.getElementsByClassName("callButton");
+for (let btn of buttonBtn) {
+  btn.addEventListener("click", function (e) {
+    const heart = document.getElementById("total-coins");
+    let total = Number(heart.innerText);
+    if (total > 19) {
+      total -= Number(20);
+      alert("colling");
+    } else {
+      alert("set massage");
+    }
+    heart.innerText = "" + total;
+    let title = btn.parentNode.parentNode.childNodes[3].innerText;
+    let num = btn.parentNode.parentNode.childNodes[7].innerText;
+    const time = new Date().toLocaleTimeString;
+  });
 }
+
 function addCopy() {
   //total-heart; total-coins; total-copy;
   const heart = document.getElementById("total-copy");
-  const total = Number(heart.innerText) + 1;
+  let total = Number(heart.innerText) + 1;
   heart.innerText = "" + total;
+}
+function clearHistory() {
+  let parent = document.getElementById("parent");
+  for (let i of parent.childNodes) {
+    parent.remove(i);
+  }
 }
